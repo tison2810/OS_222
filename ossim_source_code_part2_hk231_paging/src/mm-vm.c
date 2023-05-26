50399e5c61a5268280b933887f77a9e1c4553771
tison2810 <son.nguyenthai@hcmut.edu.vn>
2023-05-27 02:41:14 +0700

    fix find_victim_page

--- ossim_source_code_part2_hk231_paging/src/mm-vm.c
+++ ossim_source_code_part2_hk231_paging/src/mm-vm.c
@@ -507,15 +507,18 @@ int find_victim_page(struct mm_struct *mm, int *retpgn)
   struct pgn_t *pg = mm->fifo_pgn;
 
   /* TODO: Implement the theorical mechanism to find the victim page */
-  //Case 1: Queue that the victim page is in the head of the linked list
+  // //Case 1: Queue that the victim page is in the head of the linked list
   struct pgn_t *tmp = pg;
-  *retpgn = pg->pgn;
+  // *retpgn = pg->pgn;
   //Case 2: Queue that the victim page is in the tail of the linked list
 
-  // while(pg->pg_next != NULL)
-  //   pg = pg->pg_next;
-  // *retpgn = pg->pgn;
-
+  while(pg->pg_next != NULL)
+    pg = pg->pg_next;
+  while (tmp->pg_next != pg) {
+    tmp = tmp->pg_next;
+  }
+  *retpgn = pg->pgn;
+  tmp->pg_next = NULL;
   free(pg);
 
   return 0;
