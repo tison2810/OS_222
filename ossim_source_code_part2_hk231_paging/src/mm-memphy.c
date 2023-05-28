dc88963fef4b2b7593987adef2c1a57f893e0169
tison2810 <son.nguyenthai@hcmut.edu.vn>
2023-05-28 15:35:39 +0700

    update memphy

--- ossim_source_code_part2_hk231_paging/src/mm-memphy.c
+++ ossim_source_code_part2_hk231_paging/src/mm-memphy.c
@@ -183,8 +183,15 @@ int MEMPHY_dump(struct memphy_struct * mp)
     /*TODO dump memphy contnt mp->storage 
      *     for tracing the memory content
      */
-
-    return 0;
+   printf("Check memory physiscal dump\n");
+   printf("----------------------------------------\n");
+   for (int i = 0; i < mp->maxsz; i++)
+   {
+      if (mp->storage[i] != 0)
+         printf("memphy[%d] = %d\n", i, mp->storage[i]);
+   }
+   printf("----------------------------------------\n");
+   return 0;
 }
 
 int MEMPHY_put_freefp(struct memphy_struct *mp, int fpn)
